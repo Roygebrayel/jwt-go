c41a4ec70fd8c3145b9c0d6c9f6cfb8d393a51fa
Roygebrayel <roy.gebrayel11@gmail.com>
2023-10-05 14:28:33 +0300

    made the login functionality and generated jwt in a cookie .. also tested successfuuly on postman

--- controllers/usersControllers.go
+++ controllers/usersControllers.go
@@ -1,11 +1,15 @@
 package controllers
 
 import (
+	"fmt"
 	"jwt-go/initializers"
 	"jwt-go/models"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -53,4 +57,68 @@ func Signup (c *gin.Context) {
 	})
 
 
+}
+
+func Login( c *gin.Context){
+	// get the email and pass from body req
+
+	var body struct {
+		Email string
+		Password string 
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest,gin.H{
+			"error" : "no body parsed",
+		})
+		return
+	}
+
+	//lookup request user
+
+	var users models.User;
+	initializers.DB.First(&users, "email = ?", body.Email)
+
+	if users.ID == 0 {
+			
+		c.JSON(http.StatusBadRequest,gin.H{
+			"error" : "invalid email or pass",
+		})
+		return
+	}
+	
+	
+
+
+	// compare the pass with the hashed passwrod
+err := bcrypt.CompareHashAndPassword([]byte(users.Password),[]byte(body.Password))
+
+if err != nil {
+c.JSON(http.StatusBadRequest,gin.H{
+	"eror" : "there is an error",
+})
+return
+}
+	
+	// direct the user
+
+	// generate jwt token
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,jwt.MapClaims{
+		"sub" : users.ID,
+		"exp" : time.Now().Add(time.Hour* 24 * 30).Unix(),
+
+
+	})
+	tokenString, err := token.SignedString([]byte (os.Getenv("SECRET")))
+	if err!=nil {
+		fmt.Println(err);
+
+	}
+
+	// send it back
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("authorization" ,tokenString,3600 * 24 * 30 ,"" ,"",false,true )
+	c.JSON(http.StatusOK,gin.H{})
+	
 }
